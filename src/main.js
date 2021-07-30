dc8f68a6aa11d11564a6f7a6e8ad24635b6c67b7
uwaiscode <triyadi@zohomail.com>
2021-07-30 19:06:50 +0700

    bismillah

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'jquery/src/jquery.js'
+import 'popper.js/dist/popper.min.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import router from './router/index'
+//import axios
+import axios from 'axios'
+
+//set credential axios with true
+axios.defaults.withCredentials = true
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/views/home/Index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <main>
+      <div class="px-4 pt-5 my-5 text-center border-bottom">
+        <h1 class="display-4 fw-bold">Uwais Code</h1>
+        <div class="col-lg-6 mx-auto">
+          <p class="lead mb-4">
+            Ujicoba pembuatan auth dengan laravel sanctum
+          </p>
+          <p>
+            <router-link
+              :to="{ name: 'login' }"
+              v-if="!loggedIn"
+              class="btn btn-primary my-2 my-sm-0"
+              >LOGIN</router-link
+            >
+          </p>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
